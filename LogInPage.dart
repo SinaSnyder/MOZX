0092c676e6b5b0f4490dd71858dc7ee9615c9274
Sina <sinaaa733@gmail.com>
2025-05-23 13:08:44 +0330

    LogIn Updated by Sina

--- LogInPage.dart
+++ LogInPage.dart
@@ -18,60 +18,106 @@ class LoginPage extends StatelessWidget {
         title: Text('MOZX' , style: GoogleFonts.redHatDisplay(fontWeight: FontWeight.w900),),
         centerTitle: true,
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(22.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 40,),
-              Text('LOG IN' , style:  GoogleFonts.interTight(fontSize: 28 , fontWeight: FontWeight.bold , color: Colors.white),),
-              SizedBox(height: 40,),
-              TextField(
-                decoration: InputDecoration(
-                    hintText: 'Enter Username or Email',
-                    prefixIcon: Icon(Icons.person),
-                    filled: true,
-                    fillColor: Colors.grey[800],
-                    border:OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    )
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextField(
-                decoration: InputDecoration(
-                    hintText: 'Enter Password',
-                    prefixIcon: Icon(Icons.lock),
-                    filled: true,
-                    fillColor: Colors.grey[800],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    )
-                ),
-              ),
-              SizedBox(height: 20,),
-              SizedBox(
-                width: 360,
-                height: 45,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blue ,shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(13)) ),
-                    child: Text('Log in' , style: GoogleFonts.redHatDisplay(fontWeight: FontWeight.bold , fontSize: 16 , color: Colors.white),)
-                ),
-              ),
-              SizedBox(height: 10,),
-              TextButton(
-                onPressed: () {},
-                child: Text('forgot password?' , style: GoogleFonts.redHatDisplay(color: Colors.blue),),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text('or sign up' , style: GoogleFonts.redHatDisplay(color : Colors.blue , decoration: TextDecoration.underline),),
-              )
-            ],
-          )
+      body: Container(
+      decoration: BoxDecoration(
+      gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Colors.red.shade800,
+        Colors.red.shade900,
+        Colors.brown.shade900,
+        Colors.black,
+        Colors.black,
+        Colors.black,
+      ],
+    ),
+    ),
+    child: SingleChildScrollView(
+    child: Padding(
+    padding: const EdgeInsets.all(22.0),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+    SizedBox(height: 40),
+    Text(
+    'LOG IN',
+    style: GoogleFonts.interTight(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    ),
+    ),
+    SizedBox(height: 40),
+    TextField(
+    decoration: InputDecoration(
+    hintText: 'Enter Username or Email',
+    prefixIcon: Icon(Icons.person),
+    filled: true,
+    fillColor: Colors.grey[800],
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide.none,
+    ),
+    ),
+    ),
+    SizedBox(height: 20),
+    TextField(
+    decoration: InputDecoration(
+    hintText: 'Enter Password',
+    prefixIcon: Icon(Icons.lock),
+    filled: true,
+    fillColor: Colors.grey[800],
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide.none,
+    ),
+    ),
+    ),
+    SizedBox(height: 20),
+    SizedBox(
+    width: 360,
+    height: 45,
+    child: ElevatedButton(
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue,
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(13),
+    ),
+    ),
+    child: Text(
+    'Log in',
+    style: GoogleFonts.redHatDisplay(
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: Colors.white,
+    ),
+    ),
+    ),
+    ),
+    SizedBox(height: 20),
+    TextButton(
+    onPressed: () {},
+    child: Text(
+    'forgot password?',
+    style: GoogleFonts.redHatDisplay(color: Colors.blue),
+    ),
+    ),              SizedBox(height: 10),
+      TextButton(
+        onPressed: () {},
+        child: Text(
+          'or sign up',
+          style: GoogleFonts.redHatDisplay(
+            color: Colors.blue,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      ),
+    ],
+    ),
+    ),
+    ),
       ),
     );
   }
