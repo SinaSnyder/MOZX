9ddc7f7e2f9583b98cf27b7cb5e4e6e2cd5fdb82
Ilia <ilia84.m288@gmail.com>
2025-05-23 17:01:01 +0330

    Assets addes!

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mozx"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
+    namespace = "com.example.testfive"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,13 +23,18 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mozx"
+        applicationId = "com.example.testfive"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        applicationId = "com.example.testfive"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
