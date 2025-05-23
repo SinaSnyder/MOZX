49cba283c07fd1e0b6f871dfc9ed695ec3e0e257
Sina <sinaaa733@gmail.com>
2025-05-23 13:33:53 +0330

    LogIn and SignUp and Password Updated by Sina

--- SignUpPage.dart
+++ SignUpPage.dart
@@ -1,6 +1,7 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:MOZX/mozx/LogInPage.dart';
+import 'package:MOZX/mozx/password.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
@@ -8,146 +9,133 @@ class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 20, 20, 20),
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(255, 20, 20, 20),
+        elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
-        title: Text('MOZX' , style: GoogleFonts.redHatDisplay(fontWeight: FontWeight.w900),),
+        title: Text(
+          'MOZX',
+          style: GoogleFonts.redHatDisplay(
+            fontWeight: FontWeight.w900,
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: Container(
-      decoration: BoxDecoration(
-      gradient: LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      colors: [
-        Color.fromARGB(255, 184, 0, 0),
-        Color.fromARGB(255, 144, 0, 0),
-        Color.fromARGB(255, 120, 0, 0),
-        Color.fromARGB(255, 100, 0, 0),
-        Color.fromARGB(255, 75, 0, 0),
-        Color.fromARGB(255, 52, 0, 0),
-        Color.fromARGB(255, 25, 0, 0),
-        Colors.black,
-      ],
-    ),
-    ),
-    child: SingleChildScrollView(
-    child: Padding(
-    padding: const EdgeInsets.all(22.0),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-    SizedBox(height: 40),
-    Text(
-    'SIGN UP',
-    style: GoogleFonts.interTight(
-    fontSize: 28,
-    fontWeight: FontWeight.bold,
-    color: Colors.white,
-    ),
-    ),
-    SizedBox(height: 40),
-    TextField(
-    decoration: InputDecoration(
-    hintText: 'Enter Username',
-    prefixIcon: Icon(Icons.person),
-    filled: true,
-    fillColor: Colors.grey[800],
-    border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide.none,
-    ),
-    ),
-    ),
-    SizedBox(height: 20),
-    TextField(
-    decoration: InputDecoration(
-    hintText: 'Enter Email',
-    prefixIcon: Icon(Icons.email_rounded),
-    filled: true,
-    fillColor: Colors.grey[800],
-    border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide.none,
-    ),
-    ),
-    ),
-    SizedBox(height: 20),
-    TextField(
-    decoration: InputDecoration(
-    hintText: 'Enter Phone Number',
-    prefixIcon: Icon(Icons.phone),
-    filled: true,
-    fillColor: Colors.grey[800],
-    border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide.none,
-    ),
-    ),
-    ),
-    SizedBox(height: 20),
-    TextField(
-    decoration: InputDecoration(
-    hintText: 'Enter Password',
-    prefixIcon: Icon(Icons.lock),
-    filled: true,
-    fillColor: Colors.grey[800],
-    border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide.none,
-    ),
-    ),
-    ),
-    SizedBox(height: 20), SizedBox(
-        width: 360,
-        height: 45,
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blue,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(13),
-            ),
-          ),
-          child: Text(
-            'Sign up',
-            style: GoogleFonts.redHatDisplay(
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      TextButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
-          );
-        },
-        child: Text(
-          'or Log in',
-          style: GoogleFonts.redHatDisplay(
-            color: Colors.blue,
-            decoration: TextDecoration.underline,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(22.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 40),
+              Text(
+                'SIGN UP',
+                style: GoogleFonts.interTight(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 40),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Enter Username',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.person, color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Enter Email',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.email_rounded, color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Enter Phone Number',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.phone, color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              PasswordField(),
+              SizedBox(height: 20),
+              ConfirmPassword(),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 360,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(13),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign up',
+                    style: GoogleFonts.redHatDisplay(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text(
+                  'or Log in',
+                  style: GoogleFonts.redHatDisplay(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-    ],
-    ),
-    ),
-    ),
       ),
     );
   }
---
+++ password.dart
@@ -0,0 +1,88 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:MOZX/mozx/SignUpPage.dart';
+import 'package:MOZX/mozx/LogInPage.dart';
+
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({Key? key}) : super(key: key);
+
+  @override
+  _PasswordFieldState createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        hintText: 'Enter Password',
+        prefixIcon: Icon(Icons.lock),
+        filled: true,
+        fillColor: Colors.grey[850],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: _toggleVisibility,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+class ConfirmPassword extends StatefulWidget {
+  const ConfirmPassword({Key? key}) : super(key: key);
+
+  @override
+  _ConfirmPasswordState createState() => _ConfirmPasswordState();
+}
+
+class _ConfirmPasswordState extends State<ConfirmPassword> {
+  bool _obscureText = true;
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        hintText: 'Confirm Password',
+        prefixIcon: Icon(Icons.lock),
+        filled: true,
+        fillColor: Colors.grey[850],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: _toggleVisibility,
+        ),
+      ),
+    );
+  }
+}
