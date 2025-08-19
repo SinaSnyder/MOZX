427ecfefa31e82ba6f2d6df3a1aef9cbb0fcfdb5
Sina <sinaaa733@gmail.com>
2025-08-19 17:23:40 +0330

    comment codes updated

--- BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/Comment.java
+++ BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/Comment.java
@@ -1,4 +1,25 @@
 package com.example.testfive.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity @Table(name = "comment")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="song_id", length=255)
+    private String songId;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(length = 1000)
+    private String text;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private int likes = 0;
+    private int dislikes = 0;
 }
