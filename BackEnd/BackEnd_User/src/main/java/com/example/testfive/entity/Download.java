0c93e3f1cf0a272b73152168c858d309f4d719ec
Sina <sinaaa733@gmail.com>
2025-08-19 17:34:14 +0330

    comment codes updated

--- BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/Download.java
+++ BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/Download.java
@@ -1,4 +1,18 @@
 package com.example.testfive.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name = "download",
+        uniqueConstraints=@UniqueConstraint(columnNames = {"song_id","user_id"}))
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Download {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="song_id", length=255)
+    private String songId;
+
+    @Column(name="user_id")
+    private Long userId;
 }
