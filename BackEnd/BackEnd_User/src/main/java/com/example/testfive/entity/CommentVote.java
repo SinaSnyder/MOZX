c56c0b6385ce46a7bf6f926fc1a0b57c49ff59ff
Sina <sinaaa733@gmail.com>
2025-08-19 17:25:32 +0330

    comment codes updated

--- BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/CommentVote.java
+++ BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/CommentVote.java
@@ -1,4 +1,23 @@
 package com.example.testfive.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name = "comment_vote",
+        uniqueConstraints=@UniqueConstraint(columnNames = {"comment_id","user_id"}))
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class CommentVote {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="comment_id")
+    private Long commentId;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private VoteType type;
+
+    public enum VoteType { LIKE, DISLIKE }
 }
