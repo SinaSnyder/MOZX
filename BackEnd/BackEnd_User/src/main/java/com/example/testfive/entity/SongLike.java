d687fc559cd2559fbf43037b13744833a1c665c5
Sina <sinaaa733@gmail.com>
2025-08-19 17:21:51 +0330

    comment codes updated

--- BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/SongLike.java
+++ BackEnd/BackEnd_User/src/main/java/com/example/testfive/entity/SongLike.java
@@ -1,4 +1,23 @@
 package com.example.testfive.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name = "song_like",
+        uniqueConstraints=@UniqueConstraint(columnNames = {"song_id","user_id"}))
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class SongLike {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="song_id", length=255)
+    private String songId;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private VoteType type; 
+
+    public enum VoteType { LIKE, DISLIKE }
 }
