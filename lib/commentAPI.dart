8206740ff6a6271c815527f27676a3b482188598
Sina <sinaaa733@gmail.com>
2025-08-20 12:34:26 +0330

    commentAPI updated

--- lib/commentAPI.dart
+++ lib/commentAPI.dart
@@ -1,33 +1,54 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'config.dart';
-import 'PurchasePage.dart';
+import 'package:MOZX/config.dart';
+import 'package:http/http.dart' as http;
+
+class Comment {
+  final String author;
+  final String text;
+  int likes;
+  int dislikes;
+
+  Comment({
+    required this.author,
+    required this.text,
+    this.likes = 0,
+    this.dislikes = 0,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json) {
+    return Comment(
+      author: json['userId'].toString(),
+      text: json['text'],
+      likes: json['likes'],
+      dislikes: json['dislikes'],
+    );
+  }
+}
 
 Future<void> addComment(String songId, String text, int userId) async {
-  final url = Uri.parse("$baseUrl/api/shop/$songId/comments");
+  final url = Uri.parse("$baseUrl/api/comments");
   final res = await http.post(
     url,
-    headers: {
-      "Content-Type": "application/json",
-      "X-User-Id": userId.toString(),
-    },
-    body: jsonEncode({"text": text}),
+    headers: {"Content-Type": "application/json"},
+    body: jsonEncode({
+      "songId": songId,
+      "userId": userId,
+      "text": text,
+    }),
   );
-  print("Response: ${res.body}");
+
+  if (res.statusCode != 200) {
+    throw Exception("Error");
+  }
 }
 
 Future<List<Comment>> getComments(String songId) async {
-  final url = Uri.parse("$baseUrl/api/shop/$songId/comments");
+  final url = Uri.parse("$baseUrl/api/comments/$songId");
   final res = await http.get(url);
 
   if (res.statusCode == 200) {
     final List<dynamic> data = jsonDecode(res.body);
-    return data.map((c) => Comment(
-      author: c['author'],
-      text: c['text'],
-      likes: c['likes'],
-      dislikes: c['dislikes'],
-    )).toList();
+    return data.map((c) => Comment.fromJson(c)).toList();
   } else {
     throw Exception("Error");
   }
